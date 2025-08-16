bf761cc622088a0a3b8ae415da04bc4801e099b0
yankikonnagaland <yankikon.nagaland@gmail.com>
2025-08-16 12:54:59 +0530

    Revert 'Merge pull request #5 from yankikonnagaland/codex/incorporate-tailwind-css-config'

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,6 @@ import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
-import '../global.css';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
 
